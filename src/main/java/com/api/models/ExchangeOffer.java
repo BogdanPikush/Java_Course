99ec732f6cd07f221fa4012b6c913ce01cf5cc7f
BogdanPikush <120464354+BogdanPikush@users.noreply.github.com>
2024-06-16 00:25:45 +0300

    Update ExchangeOffer.java

--- src/main/java/com/api/models/ExchangeOffer.java
+++ src/main/java/com/api/models/ExchangeOffer.java
@@ -3,12 +3,50 @@ package com.api.models;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-@Document(collection = "echangeOffers")
+@Document(collection = "exchangeOffer")
 public class ExchangeOffer {
     @Id
     private String id;
     private String thingId;
     private String fromUserId;
     private String toUserId;
+
+    public ExchangeOffer(String thingId, String fromUserId, String toUserId) {
+        this.thingId = thingId;
+        this.fromUserId = fromUserId;
+        this.toUserId = toUserId;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getThingId() {
+        return thingId;
+    }
+
+    public void setThingId(String thingId) {
+        this.thingId = thingId;
+    }
+
+    public String getFromUserId() {
+        return fromUserId;
+    }
+
+    public void setFromUserId(String fromUserId) {
+        this.fromUserId = fromUserId;
+    }
+
+    public String getToUserId() {
+        return toUserId;
+    }
+
+    public void setToUserId(String toUserId) {
+        this.toUserId = toUserId;
+    }
 
 }
