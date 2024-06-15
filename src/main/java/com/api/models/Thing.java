c94dbfe3b3affcad86c0de28f859e5bd8f3b697e
BogdanPikush <120464354+BogdanPikush@users.noreply.github.com>
2024-06-16 00:22:54 +0300

    Update Thing.java

--- src/main/java/com/api/models/Thing.java
+++ src/main/java/com/api/models/Thing.java
@@ -4,7 +4,7 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(collection = "things")
-public class Things {
+public class Thing {
     @Id
     private String id;
     private String idUser;
