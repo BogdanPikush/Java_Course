de1e10f32d1f21b47e9584072f045cbc67d08e1c
BogdanPikush <120464354+BogdanPikush@users.noreply.github.com>
2024-06-16 00:26:07 +0300

    Update History.java

--- src/main/java/com/api/models/History.java
+++ src/main/java/com/api/models/History.java
@@ -1,2 +1,52 @@
-package com.api.models;public class History {
+package com.api.models;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "histories")
+public class History {
+    @Id
+    private String id;
+    private String thingId;
+    private String fromUserId;
+    private String toUserId;
+
+    public History(String thingId, String fromUserId, String toUserId) {
+        this.thingId = thingId;
+        this.fromUserId = fromUserId;
+        this.toUserId = toUserId;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getThingId() {
+        return thingId;
+    }
+
+    public void setThingId(String thingId) {
+        this.thingId = thingId;
+    }
+
+    public String getFromUserId() {
+        return fromUserId;
+    }
+
+    public void setFromUserId(String fromUserId) {
+        this.fromUserId = fromUserId;
+    }
+
+    public String getToUserId() {
+        return toUserId;
+    }
+
+    public void setToUserId(String toUserId) {
+        this.toUserId = toUserId;
+    }
 }
+
